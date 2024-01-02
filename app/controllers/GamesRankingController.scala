7a1a27b633f11b83374e7575c16b357c00093884
Bruno Nascimento <brunoenig@hotmail.com>
2024-01-02 22:15:23 +0000

    CRUD Games Ranking

--- app/controllers/GamesRankingController.scala
+++ app/controllers/GamesRankingController.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import models.Game
+import models.{Game, GameDTO}
 import play.api.libs.json.{Json, OFormat}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 
@@ -14,11 +14,55 @@ class GamesRankingController @Inject()(val controllerComponents: ControllerCompo
   gamesRanking += Game(1, "Lies of P", 8.0f)
   gamesRanking += Game(2, "Sea Of Stars", 8.0f)
 
-  implicit val todoListJson: OFormat[Game] = Json.format[Game]
+  implicit val gameRankingJson: OFormat[Game] = Json.format[Game]
+  implicit val newGame = Json.format[GameDTO]
 
-  def getAll: Action[AnyContent] = Action {
+  def ranking: Action[AnyContent] = Action {
     if(gamesRanking.isEmpty) NoContent
     else Ok(Json.toJson(gamesRanking))
+  }
+
+  def rankingPosition(positionId: Long): Action[AnyContent] = Action {
+    val gameFound = gamesRanking.find(_.positionId == positionId)
+    gameFound match {
+      case Some(game) => Ok(Json.toJson(game))
+      case None => NotFound
+    }
+  }
+
+  def updateRating(positionId: Long, newRating: Float): Action[AnyContent] = Action {
+    val index = gamesRanking.indexWhere(_.positionId == positionId)
+
+    if(index != -1) {
+      gamesRanking(index) = gamesRanking(index).copy(rating = newRating)
+    }
+
+    Ok(Json.toJson(gamesRanking(index)))
+  }
+
+  def deleteGame(positionId: Long): Action[AnyContent] = Action {
+    val index = gamesRanking.indexWhere(_.positionId == positionId)
+
+    if(index != -1) {
+      gamesRanking.remove(index)
+    }
+
+    Ok(Json.toJson(gamesRanking))
+  }
+
+  def addNewGame() = Action { implicit request =>
+    val content = request.body
+    val jsonObject = content.asJson
+    val newGameItem: Option[GameDTO] =
+      jsonObject.flatMap(Json.fromJson[GameDTO](_).asOpt)
+
+    newGameItem match {
+      case Some(gameItem) =>
+        val gameToBeAdded = Game(gameItem.positionId, gameItem.gameName, gameItem.rating)
+        gamesRanking += gameToBeAdded
+        Created(Json.toJson(gameToBeAdded))
+      case None => BadRequest
+    }
   }
 
 }
